783003c3ace6ca840d30ce66b6bee59c85006b42
Victorique Liutenko <victorique@liutenko.me>
2019-04-23 10:25:08 +0300

    add bulid

---
+++ build/precache-manifest.cdc14de7b65e4794c0f3264fb0e99f98.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac41a61bee07a17a43ca615eb134ec3e",
+    "url": "/s-pro-task/static/media/playIcon.ac41a61b.svg"
+  },
+  {
+    "revision": "e5918158977a6fc90d36",
+    "url": "/s-pro-task/static/css/main.694c7f10.chunk.css"
+  },
+  {
+    "revision": "566d5086cc168506fc88",
+    "url": "/s-pro-task/static/js/runtime~main.8da93a6f.js"
+  },
+  {
+    "revision": "2780970e549c3f944f6a",
+    "url": "/s-pro-task/static/js/2.a771dc7c.chunk.js"
+  },
+  {
+    "revision": "105805365e4c0e2e60a7ceb10ce1c1e6",
+    "url": "/s-pro-task/static/media/cloudIcon-violet.10580536.svg"
+  },
+  {
+    "revision": "072ad8400c8875304794146deac80d63",
+    "url": "/s-pro-task/static/media/cloudIcon-white.072ad840.svg"
+  },
+  {
+    "revision": "e5918158977a6fc90d36",
+    "url": "/s-pro-task/static/js/main.66a49b63.chunk.js"
+  },
+  {
+    "revision": "9713d8ed2c20b5da68a9282e173155df",
+    "url": "/s-pro-task/static/media/logoRound.9713d8ed.svg"
+  },
+  {
+    "revision": "e7522b86219f7ae4b257ccf4bd7ca90e",
+    "url": "/s-pro-task/static/media/logo.e7522b86.svg"
+  },
+  {
+    "revision": "a12b08f5383a3e203b9a880cf3ed6b77",
+    "url": "/s-pro-task/static/media/bg.a12b08f5.png"
+  },
+  {
+    "revision": "f9107813dbb5cb00b2f9a66f64df6299",
+    "url": "/s-pro-task/static/media/sliderBg.f9107813.png"
+  },
+  {
+    "revision": "ca131d0197269a8b76ed65ef9df972dd",
+    "url": "/s-pro-task/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/s-pro-task/precache-manifest.cdc14de7b65e4794c0f3264fb0e99f98.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/s-pro-task/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
